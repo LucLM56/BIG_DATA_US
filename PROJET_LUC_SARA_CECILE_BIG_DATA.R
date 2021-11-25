c12438795ce228a7cac71d7eb8fd7b0f0d4c3c77
Luc Le Mée <91048469+LucLM56@users.noreply.github.com>
2021-11-25 11:50:35 +0100

    Add files via upload
    Classification et ACP

--- PROJET_LUC_SARA_CECILE_BIG_DATA.R
+++ PROJET_LUC_SARA_CECILE_BIG_DATA.R
@@ -11,8 +11,12 @@ library(foreach)
 library(doParallel)
 library(tseries)
 library(tsoutliers)
+library(e1071)
+library(FactoMineR)
+library(cluster)
+library(NbClust) 
 
-#BDD
+##################### IMPORTATION ET PREMIERS TRAITEMENTS ##################################
 setwd("D:/Dossiers/Etudes/M2 EKAP/Big Data/Projet")
 base <- read.csv("US.csv", sep=",")
 
@@ -23,6 +27,14 @@ View(base)
 base$sasdate  <- as.Date(base$sasdate,format = "%m/%d/%Y")
 View(base)
 
+#Graphiques pour choisir la var à expliquer
+plot(base$INDPRO, type="l", main="Production industrielle") #Production industrielle
+plot(base$UNRATE, type="l", main="Taux de chomage") #Taux de chomage
+plot(base$CE16OV, type="l", main="Taux d'emploi") #Taux de d'emploi
+plot(base$CPIAUCSL, type="l", main = "Inflation") #Inflation
+plot(base$HOUST, type="l", main="Construction") #Construction
+#On sélectionne la production industrielle
+
 ##Enlever les données d'avant 1970 en raison des NA
 base2 <- base [-(1:133),]
 #Dernière ligne contient des na
@@ -32,8 +44,11 @@ summary(base2)
 base2 <- base2[,!(names(base2) %in% c("UMCSENTx","TWEXAFEGSMTHx","ACOGNO"))]
 #On retire les quelques na restants
 base2 <- na.omit(base2)
+##################################################################################################
 
-#Outliers
+
+
+############################# OUTLIERS #######################################################
 #Analyse des outliers ###
 boxplot(base2$INDPRO)
 INDPRO<- ts(data=base2$INDPRO, start=c(1970,01), frequency =12)
@@ -61,6 +76,9 @@ adf.test(base3$INDPRO) #d'après ce test la p value est strictement supérieure à 
 #Etude de la stationnarité de l'ensemble des variables explicatives 
 
 base2$INDPRO <- adj
+####################################################################################################
+
+######################### STATIONNARISER ######################################################
 #Boucle pour stationariser
 n <- ncol(base2)
 base3 = matrix(0,616,125)
@@ -82,14 +100,59 @@ cat("Nombre de variables stationnarisées : ", count, "/", n)
 base3 <- as.data.frame(base3)
 colnames(base3) <- nom_col
 
+#Export
 write.csv(base3, "D:/Dossiers/Etudes/M2 EKAP/Big Data/Projet/base_us.csv")
+#######################################################################################################"
 
-#Graphiques
-plot(base2$INDPRO, type="l", main="Production industrielle") #Production industrielle
-plot(base2$UNRATE, type="l", main="Taux de chomage") #Taux de chomage
-plot(base2$CE16OV, type="l", main="Taux d'emploi") #Taux de d'emploi
-plot(base2$CPIAUCSL, type="l", main = "Inflation") #Inflation
-plot(base2$HOUST, type="l", main="Construction") #Construction
+
+####################### STAT DESC ##################################################"
+summary(base3$INDPRO)
+sd(base3$INDPRO)
+#0.11 en moyenne avec un écart type de 0.46
+hist(base3$INDPRO, col = "blue", freq=F, xlab="INDPRO", ylab="Densité", main="Production industrielle aux USA")
+curve(dnorm(x, mean=mean(base3$INDPRO), sd=sd(base3$INDPRO)),col="red", lwd=2, add=TRUE, yaxt="n")
+#Distribution proche d'une loi normale, voir les tests 
+boxplot(base3$INDPRO, col = "blue", main="Production industrielle aux USA")
+#Quelques points extrêmes à la fin de la distribution
+#Stat de normalité :
+#skweness - coef d'asymétrie
+skewness(base3$INDPRO) 
+# -0.7090, légèrement négatif donc distribution trop décalée à droite de la médiane
+#kurtosis - coef d'applatissement
+kurtosis(base3$INDPRO) 
+# 2.3951 - La distribution est trop pointue
+################################################################################################"
+
+
+
+
+##################### CLASSIFICATION ##################################################"
+acp <- PCA(base3[,c(2:125)])
+#On oberseve des paquets de variables
+barplot(acp$eig[,2], xlab="Dim", ylab ="Percentage of variance", col = 9)
+base3cr<-scale(base3[,c(2:125)],center=T,scale=T) #Pour centrer réduire
+pol.dist <- dist(base3cr,method="euc") #Pour calculer la distance
+class0 <- hclust(pol.dist, method="ward.D2") #Pour faire les classes
+plot(as.dendrogram(class0),main="Dendrogramme") #Pour obtenir le dendogramme avec la méthode Ward.
+plot(class0, hang=-1, cex=0.8, main="Dendrogramme") #Même graphique, hang=-1 pour mettre les noms sur la même ligne. Cex pour la taille du text.
+
+#Classification difficilement interprétable
+
+#K means
+res.kmeans <- kmeans(base3cr, 30, nstart =50, algorithm = "MacQueen") #On utilise la méthode des kmeans
+res.kmeans$cluster #On visulalise les classes
+res.kmeans$centers #Les centres de gravité
+res.kmeans$betweenss #Between sum of square. Proportionel à l'inertie inter. C'est ce qu'on veut maximiser. Somme des carrés expliqués
+res.kmeans$totss #Total sum of square. 
+qual <-res.kmeans$betweenss/res.kmeans$totss
+round(qual*100,2) #Qualité = 53 %
+#######################################################################################################"
+
+
+
+###################### CORRELATION ######################################################
+corrplot(base3[,2:125], method="color")
+#######################################################################################
 
 #--------------------------------------------------------------------------------------------------------
 
